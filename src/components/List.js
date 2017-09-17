35b0f5c932533f4f9fc88ed112dc778aa460ad56
Anastasia <anastasi.smch@gmail.com>
2017-09-17 20:58:23 +0300

    use arrow function in component ListItem

--- src/components/List.js
+++ src/components/List.js
@@ -3,23 +3,21 @@ import React from 'react';
 
 import ListItem from './ListItem';
 
-function List(props) {
-	return (
-
-		<ul className='task-list'>
-			{
-				props.data.map(task => {
-					return <ListItem 
-						title={task.title} 
-						checked={task.checked} 
-						onDelete={props.onDelete}
-						onEdit={props.onEdit}
-						onCheck={props.onCheck}
-						key={task.id}
-						id={task.id} />
-				})
-			}
-		</ul>);
-}
+const List = ({data, onEdit, onDelete, onCheck}) => (
+	<ul className='task-list'>
+		{
+			data.map(task => {
+				return <ListItem 
+					title={task.title} 
+					checked={task.checked} 
+					onDelete={onDelete}
+					onEdit={onEdit}
+					onCheck={onCheck}
+					key={task.id}
+					id={task.id} />
+			})
+		}
+	</ul>
+);
 
 export default List;
