a8618b07651caecb3cdf82b160fb3bc2c8f14ecd
Anastasia <anastasi.smch@gmail.com>
2017-09-20 11:09:52 +0300

    add bindActionCreators to containers

--- src/containers/FormContainer.js
+++ src/containers/FormContainer.js
@@ -1,13 +1,14 @@
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 import { Form } from 'components';
 import { addItem } from 'actions';
 
 
-const mapDispatchToProps = dispatch => (
-  {
-    onAdd: title => dispatch(addItem(title)),
-  }
-);
+const mapDispatchToProps = dispatch => bindActionCreators({
+  onAdd: addItem
+}, dispatch);
+
 const FormContainer = connect(null, mapDispatchToProps)(Form);
 
 export default FormContainer;
--- src/containers/ListContainer.js
+++ src/containers/ListContainer.js
@@ -1,18 +1,17 @@
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import { List } from 'components';
-
-import { deleteItem, editItem, checkItem } from '../actions';
+import { deleteItem, editItem, checkItem } from 'actions';
 
 const mapStateToProps = state => ({ data: state });
 
-const mapDispatchToProps = dispatch => (
-  {
-    onDelete: id => dispatch(deleteItem(id)),
-    onEdit: (id, title) => dispatch(editItem(id, title)),
-    onCheck: id => dispatch(checkItem(id)),
-  }
-);
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  onDelete: deleteItem,
+  onEdit: editItem,
+  onCheck: checkItem
+}, dispatch);
+
 
 const ListContainer = connect(mapStateToProps, mapDispatchToProps)(List);
 
