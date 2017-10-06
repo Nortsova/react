b0c6718430618f6b5f8c9a149b36a2fa56861fc0
Anastasia <anastasi.smch@gmail.com>
2017-10-06 13:46:38 +0300

    add redux form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -5,44 +5,36 @@ import { Field, reduxForm } from 'redux-form';
 import './style.scss';
 
 class DisconnectedForm extends Component {
- state = {
-   title: '',
- }
- handleChange = ({ target }) => this.setState({ title: target.value });
-
- handleSubmit = (e) => {
-   e.preventDefault();
-
-   const { title } = this.state;
-
-   if (title) {
-     this.props.onAdd(title);
-     this.setState({ title: '' });
-   }
- }
-
 
  render = () => (
-   <form className="form" onSubmit={this.handleSubmit}>
-     <input
-       type="text"
-       value={this.state.title}
-       onChange={this.handleChange}
-       placeholder="What you should to do?"
-     />
-     <button >add task</button>
+   <form className="form" onSubmit={this.props.handleSubmit}>
+     <Field name="title" component="input" type="text" />
+     <button  type="submit">add task</button>
    </form>
  );
 }
+const NewForm = reduxForm({
+  form: 'addTask',
+})(DisconnectedForm);
 
 
-DisconnectedForm.propTypes = {
+
+class Form extends React.Component {
+  submit = (values) => {
+    const {title} = values;
+    this.props.onAdd(title);
+  }
+  render() {
+    return (
+      <NewForm onSubmit={this.submit} />
+    )
+  }
+}
+
+
+Form.propTypes = {
   onAdd: PropTypes.func.isRequired,
 };
-
-const Form = reduxForm({
-  form: 'contact',
-})(DisconnectedForm);
 
 export default Form;
 
